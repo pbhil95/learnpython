d536f8a13b9c7a16db56d8db0172f844796e4957
pbhil95 <64180320+pbhil95@users.noreply.github.com>
2020-06-13 11:19:30 +0530

    git

--- conf.py
+++ conf.py
@@ -19,6 +19,7 @@ release = ''
 html_title = 'Home'
 
 html_show_sphinx = False
+
 html_add_permalinks = ""
 # -- General configuration ---------------------------------------------------
 
@@ -87,5 +88,5 @@ html_static_path = ['_static']
 #
 # html_sidebars = {}
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-html_theme_path = os.path.join(BASE_DIR, 'learnpython')
+html_theme_path = [os.path.join(BASE_DIR, 'learnpython')]
 html_theme = 'cloud'
